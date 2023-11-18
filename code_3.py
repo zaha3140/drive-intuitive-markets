0b5edc62a0cbbb901d32268f1bfda2db1b03e189
zaha3140 <146467052+zaha3140@users.noreply.github.com>
2023-11-18 11:15:09 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,52 @@
+if __name__ == "__main__":
+
+
+import random
+    main()
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    data = generate_random_data()
+
+def generate_random_data():
+    for item in data:
+        print(f"Random Number: {item}")
+import random
+    for item in data:
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+        print(f"Random Number: {item}")
+    return data
+def main():
+    data = generate_random_data()
+    main()
+
+if __name__ == "__main__":
+        }
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+    }
+
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+}
+        }
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    for item in data:
+    main()
+
+import random
+
+        print(f"Random Number: {item}")
+    return data
+def main():
+def generate_random_data():
+
+
+    data = generate_random_data()
